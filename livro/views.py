576f97a34050d71fccfa3c7f4861947ab0a2a47a
rodrigowilton <rodrigowiltonalmeida@gmail.com>
2024-06-07 08:22:20 -0300

    alteracao 0706

--- livro/views.py
+++ livro/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from usuarios.models import Usuario
 from .models import Livros, Categoria, Emprestimos
-from .forms import CadastroLivro
+from .forms import CadastroLivro, CategoriaLivro
 
 
 
 def home(request):
     if request.session.get('usuario'):
         usuario = Usuario.objects.get(id = request.session['usuario'])
+        status_categoria = request.GET.get('cadastro_categoria')
         livros = Livros.objects.filter(usuario = usuario)
         form = CadastroLivro()
         form.fields['usuario'].initial = request.session['usuario']
         form.fields['categoria'].queryset = Categoria.objects.filter(usuario = usuario)
-        return render(request,'home.html', {'livros' : livros, 'usuario_logado': request.session.get('usuario'),'form': form})
+        form_categoria = CategoriaLivro()
+
+        return render(request,'home.html', {'livros' : livros, 'usuario_logado': request.session.get('usuario'),
+                                            'form': form, 'status_categoria': status_categoria,'form_categoria': form_categoria})
     else:
         return redirect('/auth/login/?status=2')
     
@@ -29,8 +33,10 @@ def ver_livros(request, id):
             form.fields['usuario'].initial = request.session['usuario']
             form.fields['categoria'].queryset = Categoria.objects.filter(usuario=usuario)
 
+            form_categoria = CategoriaLivro()
+
             return render(request, 'ver_livro.html',{'livro': livros, 'categoria_livro': categoria_livro, 'emprestimos': emprestimos,
-                                                     'usuario_logado':request.session.get('usuario'),'form':form, 'id_livro':id})
+                                                     'usuario_logado':request.session.get('usuario'),'form':form, 'form_categoria': form_categoria, 'id_livro': id })
         else:
             return HttpResponse('Esse livro não é seu')
     return redirect('/auth/login/?status=2')
@@ -49,3 +55,23 @@ def cadastrar_livro(request):
 def excluir_livro(request, id):
     livro = Livros.objects.get(id = id).delete()
     return redirect('/livro/home')
+
+
+def cadastrar_categoria(request):
+    if request.method == 'POST':
+        form = CategoriaLivro(request.POST)
+        if form.is_valid():
+            id_usuario = request.session.get('usuario')
+            nome = form.data['nome']
+            descricao = form.data['descricao']
+            user = request.session.get('usuario')
+            if int(id_usuario) == int(user):
+                categoria = Categoria(nome=nome, descricao=descricao, usuario_id=id_usuario)
+                categoria.save()
+                return redirect('/livro/home/?cadastro_categoria=1')
+            else:
+                return HttpResponse("Erro: Usuário inválido")
+        else:
+            return HttpResponse("Erro no formulário")
+    else:
+        return HttpResponse("Método inválido")
